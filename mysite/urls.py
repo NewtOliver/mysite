3356273788d753c101683d9ca83d29c1fcd33750
wanwu <2350686113@qq.com>
2018-12-27 13:40:20 +0800

    修改 mysite/urls.py 文件，增加配置 path('', views.index),

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
     path('index/', views.index),
     path('login/', views.login),
     path('register/', views.register),
